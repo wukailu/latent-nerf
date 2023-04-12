d8e87c04400cd8f4668a28f0d9fbdc13737469a2
wukailu <30396371+wukailu@users.noreply.github.com>
2023-04-12 13:10:29 +0800

    sync

--- src/train_lora.sh
+++ src/train_lora.sh
@@ -10,9 +10,9 @@ accelerate launch --config_file $CONFIG train_text_to_image_lora.py \
   --pretrained_model_name_or_path=$MODEL_NAME \
   --dataset_name=$DATA_DIR --caption_column="text" \
   --resolution=512 --random_flip --lora_rank=4\
-  --train_batch_size=1 \
-  --num_train_epochs=20 --checkpointing_steps=10000 \
-  --conv_learning_rate=3e-03 --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
+  --train_batch_size=2 \
+  --num_train_epochs=6 --checkpointing_steps=10000 \
+  --conv_learning_rate=1e-03 --learning_rate=1e-04 --lr_scheduler="constant" --lr_warmup_steps=0 \
   --seed=42 \
   --output_dir=$OUTPUT_DIR \
   --validation_prompt="bedroom made of cakes" --report_to="wandb"
